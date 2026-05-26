1c422546f7238a1cefb1d807cedb54c44ef7cd01
Tatiana Chavez <tatianachavezbejarano@gmail.com>
2026-05-26 18:08:14 -0500

    feat(setup): estructura base monolito en capas, shared classes y configuracion

---
+++ src/test/java/com/avh/practicas/PracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avh.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
